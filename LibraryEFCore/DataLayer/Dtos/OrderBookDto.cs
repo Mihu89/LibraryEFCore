752c152f18692329eb3784b0c1ff12b03684f8ec
Mihai <Mihail.Coscoden@icl-group.com>
2022-04-14 20:50:26 +0300

    Add pagination and Dto Example

---
+++ LibraryEFCore/DataLayer/Dtos/OrderBookDto.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Dtos
+{
+    public class OrderBookDto
+    {
+        public int BookId { get; set; }
+        public Book ChosenBook { get; set; }
+        public short BookNumbers { get; set; }
+
+        public OrderBookDto(int bookId, Book chosenBook, short bookNumbers)
+        {
+            BookId = bookId;
+            BookNumbers = bookNumbers;
+            ChosenBook = chosenBook ?? throw new ArgumentNullException(nameof(chosenBook));
+        }
+    }
+}
---
+++ LibraryEFCore/DataLayer/GenericPagination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public static class GenericPagination
+    {
+        public static IQueryable<T> Page<T>(this IQueryable<T> query, int pageNumber, int pageSize)
+        {
+            if (pageSize == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(pageSize), " pagesize cannot be zero");
+            }
+
+            if (pageNumber != 0)
+            {
+                query = query.Skip((pageNumber - 1) * pageSize);
+            }
+            return query.Take(pageSize);
+        }
+    }
+}
